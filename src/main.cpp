235832b77fc5613b193d03197cedeb2cc363829b
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2025-01-29 22:37:45 -0500

    Added base code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,50 @@
 
 /* Important to put glad first */
+#ifndef GLAD
 #include <glad/glad.h>
 #define GLAD
-#include <GLFW/glfw3.h>
+#endif
 
-int main(void) {}
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// Callback definitions
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+// Callback  implementation
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+int main(void) {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  //
+  // Callback registration
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  glViewport(0, 0, 800, 600);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+  return 0;
+}
