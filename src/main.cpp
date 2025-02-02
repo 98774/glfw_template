dc5fcec37d3bf449aa412baf2c5dc41280a6ceb1
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2025-02-01 23:30:06 -0500

    Added perspective and matricies

--- src/main.cpp
+++ src/main.cpp
@@ -67,10 +67,10 @@ int main() {
   // ------------------------------------------------------------------
   float vertices[] = {
       // positions          // colors           // texture coords
-      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 2.0f, 2.0f, // top right
-      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 2.0f, 0.0f, // bottom right
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 2.0f  // top left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
   }; // Texturing
   //  float texCoords[] = {
   //      0.0f, 0.0f, // lower-left corner
@@ -167,7 +167,6 @@ int main() {
               0);                  // set it manually
   ourShader.setInt("texture2", 1); // or with shader class
 
-  // -----------
   while (!glfwWindowShouldClose(window)) {
     // input
     // -----
@@ -186,28 +185,31 @@ int main() {
     ourShader.setFloat("mixValue", mixValue);
     ourShader.setFloat("scaleValue", scaleValue);
 
+    // create transformations
+    glm::mat4 model = glm::mat4(
+        1.0f); // make sure to initialize matrix to identity matrix first
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+    model =
+        glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    projection =
+        glm::perspective(glm::radians(45.0f),
+                         (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    // retrieve the matrix uniform locations
+    unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+    unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+    // pass them to the shaders (3 different ways)
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+    // note: currently we set the projection matrix each frame, but since the
+    // projection matrix rarely changes it's often best practice to set it
+    // outside the main loop only once.
+    //
+    unsigned int projLoc = glGetUniformLocation(ourShader.ID, "projection");
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+    //
     // Matrix  transformations
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-    trans =
-        glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    // Relocate second object
-    float scale = sin((float)glfwGetTime());
-    trans = scale * glm::mat4(1.0);
-    trans = glm::translate(trans, glm::vec3(-0.5f, -0.5f, 0.0f));
-    trans = glm::scale(trans, glm::vec3(scale, scale, scale));
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    scale = abs(cos((float)glfwGetTime()));
-    trans = scale * glm::mat4(1.0);
-    trans = glm::translate(trans, glm::vec3(0.5f, 0.5f, 0.0f));
-    trans = glm::scale(trans, glm::vec3(scale, scale, scale));
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
