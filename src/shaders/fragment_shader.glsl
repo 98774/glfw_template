ae55f91d140c1a636eaa910302127d9f76efd931
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2025-02-01 21:02:12 -0500

    First texture applied

--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -3,8 +3,12 @@ out vec4 FragColor;
 
 in vec3 ourPos;
 in vec3 ourColor;
+in vec2 texCoord;
+
+uniform sampler2D ourTexture;
 
 void main()
 {
-    FragColor = vec4(ourPos.x, ourPos.y, ourPos.z, 1.0f);
+  //    FragColor = vec4(ourPos.x, ourPos.y, ourPos.z, 1.0f);
+  FragColor = texture(ourTexture, texCoord);
 }
